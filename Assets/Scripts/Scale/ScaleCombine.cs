d8410459e37a8990ba20d388e3b0846417f563e6
deadlap <32266596+deadlap@users.noreply.github.com>
2023-03-27 15:11:27 +0200

    ting

--- Assets/Scripts/Scale/ScaleCombine.cs
+++ Assets/Scripts/Scale/ScaleCombine.cs
@@ -8,14 +8,35 @@ using UnityEditor;
 public class ScaleCombine : MonoBehaviour {
     [SerializeField] ScaleMath left;
     [SerializeField] ScaleMath right;
+    [SerializeField] GameObject pole;
     [SerializeField] GameObject finishButton;
+    [SerializeField] float rotationMax;
+
     void Start(){
         finishButton.SetActive(false);
     }
 
     void Update(){
+        float curRotation = 0;
+        float rotationOffset = 0;
         if(left.CalculateSum(out double leftSum) && right.CalculateSum(out double rightSum)) {
             finishButton.SetActive(leftSum == rightSum);
+            curRotation = -rotationMax+(float)(leftSum/rightSum)*rotationMax;
+            curRotation = (curRotation > rotationMax ? rotationMax : curRotation);
+            curRotation = (curRotation < -rotationMax ? -rotationMax : curRotation);
+            rotationOffset = -curRotation;
+        }
+        pole.transform.eulerAngles = new Vector3(
+            0,
+            0,
+            curRotation
+        );
+        foreach (Transform child in pole.transform) {
+            child.transform.localEulerAngles = new Vector3(
+                0,
+                0,
+                rotationOffset
+            );
         }
     }
 
