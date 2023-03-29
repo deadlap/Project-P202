c5d5f1fed625af05d655a2213abb6fd359fe8af5
deadlap <32266596+deadlap@users.noreply.github.com>
2023-03-29 11:32:12 +0200

    Lavet en Menu

--- Assets/Scripts/Menu/Options.cs
+++ Assets/Scripts/Menu/Options.cs
@@ -1,19 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Options : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start() {
-        
+    [SerializeField] GameObject optionsMenu;
+    [SerializeField] GameObject resetButton;
+    [SerializeField] MathInput mathInput;
+    [SerializeField] AudioSource audioSource;
+    public static bool soundState {get; private set;}
+    string equationSceneName = "EquationRandom";
+    string mainMenuName = "MainMenu";
+
+    void Start(){
+        if (SceneManager.GetActiveScene().name != equationSceneName)
+            resetButton.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update() {
-        
+    public void ToggleMenu() {
+        optionsMenu.SetActive(!optionsMenu.activeSelf);
     }
 
-    public void ChangeScene(string scene){
-        SceneManagement.StaticChangeScene(scene);
+    public void ToggleSound() {
+        soundState = !soundState;
+        audioSource.mute = soundState;
+    }
+
+    public void CallReset(){
+        if (mathInput != null) {
+            mathInput.Reset();
+            ToggleMenu();
+        }
+    }
+
+    public void ReturnToMainMenu(){
+        SceneManagement.StaticChangeScene(mainMenuName);
     }
 }
