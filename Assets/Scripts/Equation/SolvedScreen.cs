3c7d4a6f2651850018badb6172c1f16a7e7e278e
Akaidus <nikolajjensen6854@gmail.com>
2023-04-21 12:21:40 +0200

    små fix

--- Assets/Scripts/Equation/SolvedScreen.cs
+++ Assets/Scripts/Equation/SolvedScreen.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UIElements;
 
 public class SolvedScreen : MonoBehaviour {
     [SerializeField] GameObject equationSolvedScreen;
