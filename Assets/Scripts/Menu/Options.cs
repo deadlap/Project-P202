8bf9c1f9b22430faf9f130e83516d1dc22157d88
deadlap <32266596+deadlap@users.noreply.github.com>
2023-04-21 12:22:05 +0200

    fikset farve

--- Assets/Scripts/Menu/Options.cs
+++ Assets/Scripts/Menu/Options.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System;
+using UnityEngine.UI;
+
 public class Options : MonoBehaviour {
     [SerializeField] GameObject optionsMenu;
     [SerializeField] GameObject resetButton;
     [SerializeField] MathInput mathInput;
     [SerializeField] AudioSource musicSource;
+    [SerializeField] Image soundButton;
+    [SerializeField] Color enableColor;
+    [SerializeField] Color disableColor;
     public static bool soundState {get; private set;} = true;
     string mainMenuName = "MainMenu";
 
@@ -15,6 +20,16 @@ public class Options : MonoBehaviour {
         if (mathInput == null)
             resetButton.SetActive(false);
         AudioListener.volume = Convert.ToSingle(soundState);
+        enableColor = new Color32(100,255,100,200);
+        disableColor = new Color32(255,100,100,200);
+    }
+
+    void Update(){
+        if (soundState){
+            soundButton.color = enableColor;
+        } else {
+            soundButton.color = disableColor;
+        }
     }
 
     public void ToggleMenu() {
