0673306d5881bab2739ae172a41891d843f97a0d
le-kjaerulff <leonard.kjaerulff@gmail.com>
2023-05-02 17:38:34 +0200

    scales instructions

---
+++ Assets/Scripts/Scale/StartInstructions.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StartInstructions : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textDisplay;
+    [SerializeField] string[] instructionText = new string[3];
+    int i = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textDisplay.text = instructionText[i];
+    }
+
+    public void ChangeText()
+    {
+        i++;
+        if(i == 3)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            textDisplay.text = instructionText[i];
+        }
+    }
+
+
+
+
+}
