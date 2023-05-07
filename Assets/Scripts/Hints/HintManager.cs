5ad2506e3cc4c9b49db4b829bb92928175d7253d
deadlap <llabildgaard@hotmail.com>
2023-05-08 00:23:47 +0200

    sad

---
+++ Assets/Scripts/Hints/HintManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HintManager : MonoBehaviour {
+    [SerializeField] TextMeshProUGUI textDisplay;
+    [SerializeField] string[] hintText;
+
+    [SerializeField] int[] hintIDs;
+    // [SerializeField] string[] hintText;
+    int index;
+
+
+    public void PickHint(){
+
+    }
+
+    public void SelectPossibleHints(){
+
+    }
+
+
+
+}
